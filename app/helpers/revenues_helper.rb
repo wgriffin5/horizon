81ba5036ee2148de60c96fc8708e749f29b17cfb
William Griffin <williamgriffin989@gmail.com>
2015-03-14 16:55:06 -0400

    started circle, math helper

--- app/helpers/revenues_helper.rb
+++ app/helpers/revenues_helper.rb
@@ -2,7 +2,7 @@ module RevenuesHelper
 
 def calculate_pv
 
-@pv = (@revenue.gdp * @revenue.tax_take) * (1 + @revenue.growth_rate.decimal) 
+(@revenue.gdp * @revenue.tax_take) * (1 + @revenue.growth_rate.decimal) 
 
 end
 
--- app/views/revenues/circle_revenue.js
+++ app/views/revenues/circle_revenue.js
@@ -1,6 +1,15 @@
 
-var svg = d3.select().append("svg")
+var canvas = d3.select("body")
+            .append("svg")
+            .attr("width", 400)
+            .attr("height", 400);
 
-var circle = svg.selectAll("circle")
+var circle = canvas.append("circle")
+              .attr("cx", 200)
+              .attr("cy", 250)
+              .attr("r", 100)
+              .attr("fill", "blue")
+            
 
-circle.enter().append("circle")
+// circle.enter().append("circle")
+
