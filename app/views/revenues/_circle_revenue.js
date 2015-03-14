842a88aa80ad78f6453f0568c95c7240f53f02cb
William Griffin <williamgriffin989@gmail.com>
2015-03-14 17:07:39 -0400

    figuring circle

---
+++ app/views/revenues/_circle_revenue.js
@@ -0,0 +1,16 @@
+
+var canvas = d3.select("body")
+            .append("svg")
+            .attr("width", 400)
+            .attr("height", 400);
+
+var circle = canvas.selectAll("circle")
+              .attr("cx", 200)
+              .attr("cy", 250)
+              .attr("r", 100)
+              .attr("fill", "blue")
+
+circle.exit().remove();
+
+// circle.enter().append("circle")
+
