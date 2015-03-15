efed76925c8a072b92f2febd522f1db2d73c3396
William Griffin <williamgriffin989@gmail.com>
2015-03-15 16:19:43 -0400

    no console errors, but no circle

--- app/assets/javascripts/revenues.js
+++ app/assets/javascripts/revenues.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
+// $(document).ready(function() {
   
 
 
 
 
 // button calculate
-// $(document).on('click', 'revenue-create-submit', function(){
-//   console.log("creating");
-//   var revenue_id = $(this).attr('revenue_id');
-//   $.ajax({
-//     type: "POST",
-//     url: '/revenues/' + revenue_id,
-//     dataType: 'script'
-//   })
-// })
+$(document).on('click', 'revenue-create-submit', function(){
+  console.log("creating");
+  var revenue_id = $(this).attr('revenue_id');
+  $.ajax({
+    type: "POST",
+    url: '/revenues/' + revenue_id,
+    dataType: 'script'
+  })
+})
 
 
 
@@ -23,14 +23,14 @@ var svg = d3.select("body").append("svg");
 var dataInput = [30];
 
 var circle = svg.selectAll("circle")
-    .data([dataInput], function(d) { return d; });
-        .attr("cx", 50)
-        .attr("cy", 50)
-        .attr("r", function(d) { return d; })
-        .attr("fill", "blue");
+    // .data([dataInput], function(d) { return d; });
+        // .attr("cx", 50)
+        // .attr("cy", 50)
+        // .attr("r", function(d) { return d; })
+        // .attr("fill", "blue");
 
-circle.exit().remove()
-
+// circle.exit().remove();
+// }
 // circle.enter().append("circle")
 
 // Or, to resize circles in a symbol map with a staggered delay:
@@ -38,10 +38,3 @@ circle.exit().remove()
 //     .duration(750)
 //     .delay(function(d, i) { return i * 10; })
 //     .attr("r", function(d) { return Math.sqrt(d * scale); });
-
-
-
-}
-
-
-
