5ff7742345a7c29542505018d2191f6ab82eceb5
William Griffin <williamgriffin989@gmail.com>
2015-03-15 13:39:33 -0400

    circle won't display

--- app/assets/javascripts/revenues.js
+++ app/assets/javascripts/revenues.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   
 
-})
 
+
+
+// button calculate
 $(document).on('click', 'revenue-create-submit', function(){
   console.log("creating");
   var revenue_id = $(this).attr('revenue_id');
@@ -15,19 +17,31 @@ $(document).on('click', 'revenue-create-submit', function(){
 
 
 
+// Circle 
 
 var svg = d3.select("body").append("svg");
-            // .attr("width", 400)
-            // .attr("height", 400);
 var dataInput = 30;
 
 var circle = svg.selectAll("circle")
-    .data([dataInput], function(d) {return d;});
-        .attr("cx", 30)
-        .attr("cy", 30)
-        .attr("r", function(d) {return d; })
+    .data([dataInput], function(d) { return d; });
+        .attr("cx", 50)
+        .attr("cy", 50)
+        .attr("r", function(d) { return d; })
         .attr("fill", "blue");
 
 circle.exit().remove()
 
-circle.enter().append("circle")
+// circle.enter().append("circle")
+
+// Or, to resize circles in a symbol map with a staggered delay:
+// d3.selectAll("circle").transition()
+//     .duration(750)
+//     .delay(function(d, i) { return i * 10; })
+//     .attr("r", function(d) { return Math.sqrt(d * scale); });
+
+
+
+})
+
+
+
