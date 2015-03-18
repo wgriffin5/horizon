b4f4b9b7c04fa714eb0db89ac969df1bd9bcc8ab
William Griffin <williamgriffin989@gmail.com>
2015-03-18 00:18:48 -0400

    form submit class, js

--- app/assets/javascripts/revenues.js
+++ app/assets/javascripts/revenues.js
@@ -1,76 +1,62 @@
 $(document).ready(function() {
 
 
-// Circle 
-var svg = d3.select("body").append("svg");
-var dataInput = 30;
-var circle = svg.selectAll("circle")
-    .data([dataInput], function(d) { return d; });
-    circle.attr("cx", 50)
-    circle.attr("cy", 50)
-    circle.attr("r", function(d) { return d; })
-    circle.attr("fill", "blue")
-    console.log("CIRCLE")
 
-// circle.exit().remove();
-
+  // var svg = d3.select("body").append("svg");
+  // var dataInput = (gdp*taxtake*growthrate);
+  // var dataInput = 50
   
 
-$('form').on('submit', function(e) {
-  event.preventDefault();
-  var form = $(this);
-  var request = {
-    name: $('#revenue_name').val(),
-    gdp: $('#revenue_gdp').val(),
-    taxtake: $('#revenue_tax_take').val(),
-    growthrate: $('#revenue_growth_rate').val()
-  };
-  console.log(request);
-  $.ajax('/revenues(.json)', {
-    type: 'POST',
-    contentType: 'application/json',
-    dataType: 'json',
-    data: request
-    success: function(result) {
-      console.log(result);
-    }
-    error: function(err){
-      console.log(err);
-    }
-
-  }
-
-
+  // var circle = svg.selectAll("circle")
+  //     .data([dataInput], function(d) { return d; });
+  //     circle.attr("cx", 50)
+  //     circle.attr("cy", 50)
+  //     circle.attr("r", function(d) { return d; })
+  //     circle.attr("fill", "blue")
+  //     console.log("CIRCLE")
+  // // circle.exit().remove();
 
 
 
   
-});
+
+  $('#form').on('submit', function(e) {
+    event.preventDefault();
+
+    var request = {
+      name: $('#revenue_name').val(),
+      gdp: $('#revenue_gdp').val(),
+      taxtake: $('#revenue_tax_take').val(),
+      growthrate: $('#revenue_growth_rate').val()
+    };
+    console.log(request);
+    $.ajax('/revenues(.json)', {
+      type: 'POST',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: request,
+      success: function(result) {
+        var msg = $('<p></p>');
+        msg.append("post works");
+        console.log(result);
+        $('form').append('svg'),
+      error: function(err){
+        console.log(err);
+      };
+    // $.ajax('/revenues/:id/(.json)', {
+    //     type: 'GET',
+    //     contentType: 'application/json',
+    //     dataType: 'json',
+    //     data: request,
+    //     success: 
+    // });
   });
 
 });
-// } )
-
-$('form').on('submit', function(e){
-  event.preventDefault();
 
 
-// button calculate // def calculate method
-// $(document).on('click', 'revenue-create-submit', function(){
-//   console.log("creating");
-//   var revenue_id = $(this).attr('revenue_id');
-//   $.ajax({
-//     type: "POST",
-//     url: '/revenues/' + revenue_id,
-//     dataType: 'script'
-//   })
 
 
-// }
-// circle.enter().append("circle")
 
-// Or, to resize circles in a symbol map with a staggered delay:
-// d3.selectAll("circle").transition()
-//     .duration(750)
-//     .delay(function(d, i) { return i * 10; })
-//     .attr("r", function(d) { return Math.sqrt(d * scale); });
+
+
