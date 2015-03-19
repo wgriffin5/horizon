7754ac9ee6692fa0600f71704afe1ad9afabdbef
William Griffin <williamgriffin989@gmail.com>
2015-03-19 16:31:17 -0400

    persistent circles, different button, circles still form-independent

--- app/assets/javascripts/revenues.js
+++ app/assets/javascripts/revenues.js
@@ -1,52 +1,49 @@
 $(document).ready(function() {
 
-var dataInput = 20;
+  var create = function(d) {   
 
-var create = function(d) {   
+  // var gdp = $('#revenue_gdp').val(),
+  // var taxtake = $('#revenue_tax_take').val(),
+  // var growthrate = $('#revenue_growth_rate').val();
 
-// var gdp = $('#revenue_gdp').val(),
-// var taxtake = $('#revenue_tax_take').val(),
-// var growthrate = $('#revenue_growth_rate').val();
+  var gdp = 3
+  var taxtake = 5
+  var growthrate = 10
 
-var gdp = 3
-var taxtake = 5
-var growthrate = 8
+  var number = (+gdp * +taxtake * +growthrate);
 
-var number = (+gdp * +taxtake * +growthrate);
+  var svg = d3.select("#body").append("svg")
+            .attr('height', 400)
+            .attr('width', 400);
 
-var svg = d3.select("#canvas").append("svg")
-          .attr('height', 400)
-          .attr('width', 400);
-var circle = svg.append("circle")
-              circle.attr("cx", 200)              
-              circle.attr("cy", 200)
-              circle.attr("r", number )
-              .style('fill', 'blue');
-}
+  var circle = svg.append("circle")
+                circle.attr("cx", 200)              
+                circle.attr("cy", 200)
+                circle.attr("r", number )
+                .style('fill', 'blue')
+                circle.exit().remove();
+  }
+
+// create()
 
 
-create()
+    $('.circle-button').on('click', function() {
+      event.preventDefault
+      create()
+      console.log(CLICKO)
+  })
+
+      // event.preventDefault()
+      // console.log(request);
+      // $.ajax('/revenues(.json)', {
+      //   type: 'POST',
+      //   contentType: 'application/json',
+      //   dataType: 'json',
+      //   data: request,
+      //   success: function() {
+      //     create()
 
 })
-
-
-
-
-  
-
-//   $('form').on('submit', function(e) {
-//     event.preventDefault()
-//     console.log(request);
-//     $.ajax('/revenues(.json)', {
-//       type: 'POST',
-//       contentType: 'application/json',
-//       dataType: 'json',
-//       data: request,
-//       success: function() {
-        // create()
-//         }
-
-
 
 
 
