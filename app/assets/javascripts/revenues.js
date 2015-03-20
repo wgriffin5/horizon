91574918f711beafc2c4c348afd223b23c0bfed1
William Griffin <williamgriffin989@gmail.com>
2015-03-20 16:08:06 -0400

    ditched other button, 400 post bad request error

--- app/assets/javascripts/revenues.js
+++ app/assets/javascripts/revenues.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
 
-  var create = function(d) {   
+  var create = function() {   
 
+  var number = 150
   // var gdp = $('#revenue_gdp').val(),
   // var taxtake = $('#revenue_tax_take').val(),
   // var growthrate = $('#revenue_growth_rate').val();
+  // var calculate = function(c) {
+  //   var gdp = 3
+  //   var taxtake = 5
+  //   var growthrate = 12
+  //   var result = ()
 
-  var gdp = 3
-  var taxtake = 5
-  var growthrate = 10
+  // } { result r; };
+  
 
-  var number = (+gdp * +taxtake * +growthrate);
+  // var radius = calculate() 
+  
 
   var svg = d3.select("#body").append("svg")
             .attr('height', 400)
@@ -19,34 +25,32 @@ $(document).ready(function() {
   var circle = svg.append("circle")
                 circle.attr("cx", 200)              
                 circle.attr("cy", 200)
-                circle.attr("r", number )
-                .style('fill', 'blue')
-                
+                circle.attr("r", number)
+                .style('fill', 'blue');
   }
 
-// create()
 
+  $('.button').on('click', function() {
+      event.preventDefault();
+      $.ajax('/revenues.json', {
+        type: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: { format: 'json'},
+       success: function() { 
+        create()
+      }
+       });
 
-    $('.circle-button').on('click', function() {
-      create()
-  })
-
-      // event.preventDefault()
-      // console.log(request);
-      // $.ajax('/revenues(.json)', {
-      //   type: 'POST',
-      //   contentType: 'application/json',
-      //   dataType: 'json',
-      //   data: request,
-      //   success: function() {
-      //     create()
-
+      // console.log("button");
+})
+  
 })
 
 
-
-
-
-
-
-
+// Use an AJAX post to submit your form
+// $.post( 
+//      sucess: function() {
+//        draw your circle
+//      }
+// )
