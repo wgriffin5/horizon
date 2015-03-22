0f094da19d8712eef5f976f590bdcf4ffa5682e3
William Griffin <williamgriffin989@gmail.com>
2015-03-21 23:57:05 -0400

    nvm, go back to regular form, focus on circles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314003353) do
+ActiveRecord::Schema.define(version: 20150322034414) do
 
   create_table "revenues", force: :cascade do |t|
     t.string   "name",        limit: 255
