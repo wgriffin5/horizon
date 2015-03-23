65c174f9b495bba4c31c4bca3397b58061a4d617
William Griffin <williamgriffin989@gmail.com>
2015-03-23 12:13:28 -0400

    asdf

--- app/assets/javascripts/revenues.js
+++ app/assets/javascripts/revenues.js
@@ -1,33 +1,102 @@
 $(document).ready(function() {
 
-var create = function(d) {
+
+
+
+// var gdpCircle = function(d) {
+
+//   var gdp = $('#gdp').val();
+  
+//   var gdpRadius = gdp
+
+// var svg = d3.select("#body").append("svg")
+//             .attr('id','svg_id')
+//             .attr('height', 400)
+//             .attr('width', 400);
+
+//   var circle = svg.append("circle")
+
+//                 circle.attr('id', 'circle_id')
+//                 circle.attr("cx", 200)              
+//                 circle.attr("cy", 200)
+//                 circle.attr("r", gdpRadius)
+//                 .style('fill', rgba(0, 102, 255, 0.3) );
+
+// /// debt/gdp pie chart 
+// }
+
+
+// var debtCircle = function(d) {
+
+//   var gdp = $('#gdp').val();
+//   var debt = (gdp * .6)
+
+//   var svg = d3.select("#body").append("svg")
+//             .attr('id','svg_id')
+//             .attr('height', 400)
+//             .attr('width', 400);
+
+//   var circle = svg.append("circle").enter()
+
+//                 circle.attr('id', 'circle_id')
+//                 circle.attr("cx", 200)              
+//                 circle.attr("cy", 200)
+//                 circle.attr("r", debt)
+//                 .style('fill', 'red');
+// }
+
+var present = function(d) {
  
-  var gdp = $('#gdp').val();
-  var taxtake = $('#taxtake').val();
-  var growthrate = $('#growthrate').val();
+
+var svg = d3.select("body").append("svg");
+var dataInput = 40;
+
+var circle = svg.selectAll("circle")
+    .data([dataInput], function(d) { return d; });
+        .attr("cy", 70)
+        .attr("cx", 70)
+        .attr("r", function(d) { return d; });
+
+circle.exit().remove()
 
 
- var number = ((gdp*taxtake)/(taxtake-growthrate)) * (1 - ((1+growthrate)/(1+taxtake))^1000)
 
-  var svg = d3.select("#body").append("svg")
-            .attr('id','svg_id')
-            .attr('height', 400)
-            .attr('width', 400);
+//   var gdp = $('#gdp').val();
+//   var taxtake = $('#taxtake').val();
+//   var growthrate = $('#growthrate').val();
 
-  var circle = svg.append("circle")
-                circle.attr('id', 'circle_id')
-                circle.attr("cx", 200)              
-                circle.attr("cy", 200)
-                circle.attr("r", number)
-                .style('fill', 'blue');
+//   var left = (gdp * (taxtake/100))/((taxtake/100)-(growthrate/100))
+
+//   var right = 1 - ((1 + (growthrate/100))/(1 + (taxtake/100)))^1000
+
+//   var revenueRadius = (left * right)/250
+
+//   // ((gdp*(taxtake/100))/((taxtake/100)-(growthrate/100)} * {1 - ((1+(growthrate/100))/(1+(taxtake/100)))^1000}
+
+
+//   var svg = d3.select("#body").append("svg")
+//             .attr('id','svg_id')
+//             .attr('height', 400)
+//             .attr('width', 400);
+
+//   var circle = svg.append("circle")
+//                 circle.attr('id', 'circle_id')
+//                 circle.attr("cx", 200)              
+//                 circle.attr("cy", 200)
+//                 circle.attr("r", revenueRadius)
+//                 .style('fill', 'blue');
+
 }
-  
-  
-// $('a').click(function(event){
-//     event.preventDefault(); 
-//   });
 
-  $('a').click(function(event) {
+// var debtButton = function(d) {
+//   var debt = %a.btn-3d.red{:href => "#"} Current Debt
+
+// }
+
+  
+
+
+  $('#revenue-button').click(function(event) {
       event.preventDefault();
       console.log('REVENUEEE')
       var name = $('#name').val();
@@ -42,12 +111,36 @@ var create = function(d) {
         dataType: 'json',
         data: {revenue: { name: name, gdp: gdp , tax_take: taxtake , growth_rate: growthrate}},
        complete: function() { 
-        create()
+        present()
+
        }
 
-
       });
+  
+  })
 
+  // $('#revenue-button').click(function(event){
+  //   event.preventDefault();
+  //   console.log('RevenueTransition!')
+  //   present().transition().attr('r', revenueRadius );
+
+  // })
+
+
+  $('#debt-button').click(function(event) {
+    event.preventDefault();
+    console.log('DEBT')
+    debtCircle()
+  })
+
+  $('#gdp-button').click(function(event) {
+    event.preventDefault();
+    console.log('GDPPP')
+    gdpCircle()
   })
 
 })
+
+
+
+
